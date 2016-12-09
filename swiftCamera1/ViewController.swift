c834559ff77cba632c0163e8f490b0b3af5b88b8
ftwdjw <ftwdjw@gmail.com>
2016-12-09 12:14:31 -0600

    initial commit

--- swiftCamera1/ViewController.swift
+++ swiftCamera1/ViewController.swift
@@ -1,25 +1,65 @@
 //
 //  ViewController.swift
-//  swiftCamera1
+//  CameraApplication
 //
-//  Created by John Mac on 12/9/16.
-//  Copyright © 2016 John Wetters. All rights reserved.
+//  Created by John Mac on 12/16/15.
+//  Copyright © 2015 John Wetters. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {//begin
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad() {//begin
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        
+        
+        
+    }//end
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {//begin
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }//end
+    
+    @IBAction func chooseImageFromPhotoLibrary() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+        print("choose existing photo")
     }
+    
+    
+    @IBAction func chooseFromCamera(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        
+        present(picker, animated: true, completion: nil)
+        
+        print("take photo with camera")
 
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+     
+    }
+    
+    
 
-}
+}//end
 
